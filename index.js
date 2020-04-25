1cb6a18b09b4fc500a3d455f3c8dc69e246f9c48
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-04-25 20:07:19 +0530

    done

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
+var server = http.createServer(handelServer);
 
-var server = http.createServer(handelServer)
+function handelServer(req, res) {
 
+  var arr = ["png", "jpg"];
+//   var parsedUrl = parse(req.url)
 
-function handelServer(req, res){
+  if (req.url.includes("html")) {
+    res.writeHead(201, { "content-Type": "text/html" });
+    fs.createReadStream(`.${req.url}`).pipe(res);
+  }
 
-    if(req.url === '/') {
-        res.writeHead(201, {'content-Type': 'text/html'})
-        fs.createReadStream("./index.html").pipe(res)
+  if (req.url === "/assets/stylesheet/style.css") {
+    res.writeHead(201, { "content-Type": "text/css" });
+    fs.createReadStream("./assets/stylesheet/style.css").pipe(res);
+  } else {
+    for (var i = 0; i < arr.length; i++) {
+      if (req.url.endsWith(arr[i])) {
+        res.writeHead(201, { "content-Type": "images/*" });
+        fs.createReadStream(`.${req.url}`).pipe(res);
+        break;
+      }
     }
-
-    if(req.url === '/assets/stylesheet/style.css'){
-        res.writeHead(201, {'content-Type' : 'text/css'})
-        fs.createReadStream('./assets/stylesheet/style.css').pipe(res)
-    }
-
+  }
 }
 
-server.listen(3000, ()=> console.log('Server Start'))
+server.listen(3000, () => console.log("Server Start"));
