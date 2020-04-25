3aa09118d131f255365a057dc1aac46993c28f02
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-04-25 19:42:45 +0530

    css file add

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ var fs = require('fs')
 var server = http.createServer(handelServer)
 
 
-server.listen(4000, ()=> console.log('Server Start'))
+function handelServer(req, res){
+
+    if(req.url === '/') {
+        res.writeHead(201, {'content-Type': 'text/html'})
+        fs.createReadStream("./index.html").pipe(res)
+    }
+
+    if(req.url === '/assets/stylesheet/style.css'){
+        res.writeHead(201, {'content-Type' : 'text/css'})
+        fs.createReadStream('./assets/stylesheet/style.css').pipe(res)
+    }
+
+}
+
+server.listen(3000, ()=> console.log('Server Start'))
